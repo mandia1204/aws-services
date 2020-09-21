0dc4604a1abe7f6838273f72ec295d9acf5a977e
Marvin Andia <mandia1204@gmail.com>
2020-09-20 19:12:04 -0500

    adding code commit event, and sam token authorizer

---
+++ CodeCommitEvent/Event.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CodeCommitEvent
+{
+    public class Event
+    {
+        public string id { get; set; }
+        [JsonPropertyName("detail-type")]
+        public string detailType { get; set; }
+        public string source { get; set; }
+        public EventDetail detail { get; set; }
+    }
+
+    public  class EventDetail
+    {
+        [JsonPropertyName("event")]
+        public string eventName { get; set; }
+        public string repositoryId { get; set; }
+        public string commitId { get; set; }
+    }
+}
---
+++ sam/testing/token-validator/app.js
@@ -0,0 +1,49 @@
+const axios = require('axios').default;
+const jwt_decode = require('jwt-decode');
+const jwkToPem = require('jwk-to-pem'),
+    jwt = require('jsonwebtoken');
+
+const publicKeysUrl = 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_05E5JDNR9/.well-known/jwks.json';
+let response;
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const { kid } = jwt_decode(event.authorizationToken, { header: true});
+        const { data } = await axios(publicKeysUrl);
+        const jwk = data.keys.find(k => k.kid === kid);
+
+        const pem = jwkToPem(jwk);
+        const options = {
+            audience: '2gkt10rpuddamt5gpnlmc2j8l4',
+            issuer: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_05E5JDNR9',
+            ignoreExpiration: false
+        };
+        const decodedToken = jwt.verify(event.authorizationToken, pem, options);
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+                decodedToken
+                // location: ret.data.trim()
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
